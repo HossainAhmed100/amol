6f5143bffc2040164b8fa4d5b7b1259f2bb61cce
HossainAhmed100 <successfulllife101@gmail.com>
2024-06-18 02:55:19 +0600

    Added Bittom Tab Screen

---
+++ components/TaskList.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const TaskList = () => {
+  return (
+    <View style={styles.cardContainer}>
+        <Text style={styles.taskTitle}>New Dua</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    cardContainer: {
+      paddingHorizontal: 20,
+      paddingVertical: 20,
+      backgroundColor: '#c8f49ba6',
+      borderRadius: 20,
+      marginVertical: 10,
+      marginHorizontal: 10,
+      shadowColor: '#000',
+    },
+    taskTitle: {
+      fontSize: 20,
+      color: '#000',
+    }
+});
+
+export default TaskList
